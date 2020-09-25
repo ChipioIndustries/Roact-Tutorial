fd1206efa7f2818016b919c6107142767b17cf07
Chipio Industries <chipioindustries@gmail.com>
2020-09-25 10:50:21 -0700

    Fixed name prop

--- src/StarterPlayer/StarterPlayerScripts/03-stateful-components/Greeting.lua
+++ src/StarterPlayer/StarterPlayerScripts/03-stateful-components/Greeting.lua
@@ -6,8 +6,7 @@ function Greeting:render()
 	return Roact.createElement("ScreenGui",{
 		Name = "Interface";
 	},{
-		Roact.createElement("TextLabel",{
-			Name = self.props.name;
+		[self.props.name] = Roact.createElement("TextLabel",{
 			Size = UDim2.new(0,200,0,50);
 		})
 	})
